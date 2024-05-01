415bc1932a11d5b69b1b6b92724f840ad0922970
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2024-05-01 18:05:56 +0700

    example main โดยรวมมันทุกอย่างที่เรียน 5555

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 
 	"github.com/bxcodec/faker/v4"
 	"github.com/google/uuid"
+	"github.com/idontknowtoobrother/go-example/datastructure"
 	"github.com/idontknowtoobrother/go-example/grade"
 	"github.com/idontknowtoobrother/go-example/hexafu"
+	"github.com/idontknowtoobrother/go-example/leetcode"
 	"github.com/idontknowtoobrother/go-example/loop"
 	"github.com/idontknowtoobrother/go-example/pracconst"
 )
@@ -33,4 +35,29 @@ func main() {
 	loop.ForLoop()
 	loop.DoWhileLoop()
 	loop.WhileLoop()
+
+	fmt.Println()
+	fmt.Println(datastructure.GetStructure("array"))
+	fmt.Println(datastructure.GetStructure("slice"))
+	datastructure.ConvertArrayToSlice()
+
+	gradeMap := datastructure.GetIntMap()
+	myScore := 20
+	myGrade := getGradeWithGradeMap(gradeMap, myScore)
+	fmt.Println("\nGetIntMap=", gradeMap)
+	fmt.Println("My score=", myScore)
+	fmt.Println("My Grade=", myGrade)
+
+	datastructure.RemoveItemInMapExample()
+
+	fmt.Println("TwoSum: nums={1,4,5,2} target=6 / result=", leetcode.TwoSum([]int{1, 4, 5, 2}, 6))
+}
+
+func getGradeWithGradeMap(gradeMap map[int]string, score int) string {
+	for moreThan, gradeChar := range gradeMap {
+		if score >= moreThan {
+			return gradeChar
+		}
+	}
+	return "F"
 }
